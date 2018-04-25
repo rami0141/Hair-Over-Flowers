5784e78ad129b27c5f12c725f25ddb81104a8de7
jaethao <mjathao@gmail.com>
2018-04-25 16:31:15 -0500

    added react app

---
+++ client/src/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Date from "./components/Date";
+
+const App = () => <Date />;
+
+export default App;
---
+++ client/src/components/Date.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+
+const Date = () => <p>Hello World!</p>;
+
+export default Date;

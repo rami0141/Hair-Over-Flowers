b380306c78e1e70d446291309998e556c4b9c24b
jaethao <mjathao@gmail.com>
2018-05-01 15:11:50 -0500

    styled admin

--- public/assets/js/clndr.js
+++ public/assets/js/clndr.js
@@ -42,11 +42,11 @@
     var clndrTemplate =
         "<div class='clndr-controls'>" +
             // "<div class='clndr-control-button'>" +
-                "<span class='clndr-previous-button'>prev</span>" +
+                "<span class='clndr-previous-button'><</span>" +
             // "</div>" +
             "<span class='month'><%= month %> <%= year %></span>" +
             // "<div class='clndr-control-button rightalign'>" +
-                "<span class='clndr-next-button'>next</span>" +
+                "<span class='clndr-next-button'>></span>" +
             // "</div>" +
         "</div>" +
         "<table class='clndr-table' border='0' cellspacing='0' cellpadding='0'>" +

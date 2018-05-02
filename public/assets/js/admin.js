9a64667ebbd216236c9f5e585fa746babbd02b5b
Cristina Zhang <rami0141@umn.edu>
2018-05-02 13:03:41 -0500

    code

--- public/assets/js/admin.js
+++ public/assets/js/admin.js
@@ -22,45 +22,48 @@ $(document).ready(function() {
             stylistName = data.stylistName;
             // displays the stylist name in the admin page
             $("#message").append("Hello " + stylistName + "!");
-            console.log(stylistName);
+            // Calling getAppointments Function
             getAppointments();
-
-            // Displays Stylist Image Dynamically
-            var stylistContainer = document.getElementById('showImageHere');
-            var img1 = document.createElement('img');
-            $(img1).addClass("stylistImage");
-            
-            if (stylistName == "Mika Tan") {
-                console.log("This is Mika")
-                img1.src = "assets/images/stylist1.png";
-            }
-            else {
-                img1.src = "assets/images/stylist2.png";
-            }
-
-            stylistContainer.appendChild(img1);
-
+            displayImage();
         });
     }
 
+    // Displays Stylist Image Dynamically
+    function displayImage() {
+        //console.log(stylistName)
+        
+        var stylistContainer = document.getElementById('showImageHere');
+        var img1 = document.createElement('img');
+        $(img1).addClass("stylistImage");
 
+        if (stylistName == "Mika Tan") {
+            console.log("This is Mika")
+            img1.src = "assets/images/stylist1.png";
+        }
+        else {
+            img1.src = "assets/images/stylist2.png";
+        }
+
+            stylistContainer.appendChild(img1);
+    }
 
     // -----------------------------------------------------------------
     // Pull Only one stylist appointments
 	function getAppointments() {
+    	$.get("/api/appointments/" + stylistName, function (data) {
+            appointmentArray = data;
+           // console.log("Array", appointmentArray);
+            loopingAppointments(appointmentArray);
+           // console.log(appointmentArray[2].name);
+        });
+      };
 
-	$.get("/api/appointments/" + stylistName, function (data) {
-        appointmentArray = data;
-       // console.log("Array", appointmentArray);
-        loopingAppointments(appointmentArray);
-       // console.log(appointmentArray[2].name);
-    });
-  };
-
+    // ----------------------------------------------------------------
 	// This function will loop through all appointments and display only the appointments with todays date
     function loopingAppointments(appointmentArray) {
     	for (var i = 0; i < appointmentArray.length; i++) {
     		// declaring variables
+            var iD = appointmentArray[i].id
     		appTime = appointmentArray[i].appTime;
     		name = appointmentArray[i].name;
     		email = appointmentArray[i].email;
@@ -78,7 +81,7 @@ $(document).ready(function() {
 
                 // dynamically creates table - The comments section is commented out
         		$("#time").append("<tr><td>" + appTime + "</td></tr>");
-        		$("#name").append("<tr><td><i class='fas fa-comment fa-lg' data-toggle='popover' title='Comment' data-content='And here is some amazing content. It is very engaging. Right?'></i> " + name+ "</td></tr>");
+        		$("#name").append("<tr><td><i class='fas fa-comment fa-lg' data-toggle='popover" + i + " 'title='Comment' data-content=" + comments + "'And here is some amazing content. It is very engaging. Right?'></i> " + name+ "</td></tr>");
         		// $("#email").append("<tr><td>" + email + "</td></tr>");
         		$("#number").append("<tr><td>" + number + "</td></tr>");
         		$("#service").append("<tr><td>" + service + "</td></tr>");
@@ -86,22 +89,27 @@ $(document).ready(function() {
                 $("#comments").append("<tr><td>" + comments + "</td></tr>");
         		//$("#button").append("<tr><td><button class='btn btn-primary btn-sm delete" + i + "' type='button'>Check-In</button></td></tr>");
     			$("#button").append("<tr><td><input class='form-check-input delete' type='checkbox' id='defaultCheck1'><label class='form-check-label' for='defaultCheck1'>Check</label></td></tr>");
-
-    	   }
+    	   
+           }
         }
     }// End of loopingAppointments functions
 
 
 
-
+    // -------------------------------------------------------------------
     //Show appointments by month for each stylist
-    $('select').on('change', function (){
+    $('select').on('change', function(){
+        monthSelected = this.value;
+        appointmentsByMonth(monthSelected);
+    });
+
+    function appointmentsByMonth(monthSelected) {
         $("#month").empty();
         $("#monTime").empty();
         $("#monName").empty();
         $("#deleteApp").empty();
-        monthSelected = this.value;
 
+        getAppointments();
         for (var i = 0; i < appointmentArray.length; i++) {
             if (appointmentArray[i].appMonth == monthSelected) {
                 newArr.push(appointmentArray[i]);
@@ -111,6 +119,7 @@ $(document).ready(function() {
                 appDay = appointmentArray[i].appDay;
                 appTime = appointmentArray[i].appTime;
                 name = appointmentArray[i].name;
+                console.log("Hello there")
 
                 $("#month").append("<tr><td>" + appMonth + "-" + appDay + "</td></tr>");
                 $("#monTime").append("<tr><td>" + appTime + "</td></tr>");
@@ -118,18 +127,21 @@ $(document).ready(function() {
                 $("#deleteApp").append("<tr><td><button type='button' class='btn btn-danger btn-sm popoover'" + i + "'' id='deleteAppointment' value='" + I_D + "'>Delete</button></td></tr>");
             }
         }
-    });
+    };
 
+    // --------------------------------------------------------------------
     // Function for handling what happens when the delete button is pressed
     $(document).on("click", "#deleteAppointment", function() {
+
         console.log("Hello" + this.value);
         id = this.value;
         console.log(id);
+
         $.ajax({
           method: "DELETE",
           url: "/api/appointments/" + id
-        })
-      });
-
-
-    }); // end of document.ready function
+        }).done(function(){
+            appointmentsByMonth(monthSelected);
+        });
+    });
+}); // end of document.ready function
